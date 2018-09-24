9eb4e95223d2aff02b362ee957b0a8b262372578
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-09-23 22:35:25 -0400

    All

---
+++ pig-latin.js
@@ -0,0 +1,21 @@
+function translatePigLatin(str) {
+    let word = str.split("");
+    let matchAgainst = /[^aeiouy]{0,5}/i;
+    let consonants = str.match(matchAgainst);
+    if(consonants[0] == ""){
+        str += "way";
+        
+        return str;
+    }
+    str = str.replace(consonants[0] , "");
+    str += consonants[0]+"ay";
+    
+    return str;
+}
+
+translatePigLatin("Consonant");
+//Pig Latin takes the first consonant (or consonant cluster) of an English word,
+// moves it to the end of the word and suffixes an "ay".
+
+//If a word begins with a vowel you just add "way"
+//to the end.
