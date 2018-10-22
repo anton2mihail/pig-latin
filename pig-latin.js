eee036b179307b0b0aadb114b9868127adad53e4
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-22 18:30:33 -0400

    Refactored for lhl

--- pig-latin.js
+++ pig-latin.js
@@ -1,6 +1,18 @@
 // Pig Latin takes the first consonant (or consonant cluster) of an English
 // word, moves it to the end of the word and suffixes an "ay". If a word begins
 // with a vowel you just add "way" to the end.
+
+function input() {
+    var args = process
+        .argv
+        .slice(2);
+
+    var result;
+    for (let i = 0; i < args.length; i++) {
+        result += translatePigLatin(args[i]) + " ";
+    }
+    console.log(result);
+}
 function translatePigLatin(str) {
     let word = str.split("");
     let matchAgainst = /[^aeiouy]{0,20}/i;
@@ -16,4 +28,4 @@ function translatePigLatin(str) {
     return str;
 }
 
-translatePigLatin("Cccccccconsonant");
+input();
