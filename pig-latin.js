3450d4981a0a9baa4fe8217aa97d2396cdf63fa2
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-22 18:24:23 -0400

    No changes

--- pig-latin.js
+++ pig-latin.js
@@ -1,21 +1,19 @@
 function translatePigLatin(str) {
     let word = str.split("");
-    let matchAgainst = /[^aeiouy]{0,5}/i;
+    let matchAgainst = /[^aeiouy]{0,20}/i;
     let consonants = str.match(matchAgainst);
-    if(consonants[0] == ""){
+    if (consonants[0] == "") {
         str += "way";
-        
+
         return str;
     }
-    str = str.replace(consonants[0] , "");
-    str += consonants[0]+"ay";
-    
+    str = str.replace(consonants[0], "");
+    str += consonants[0] + "ay";
+
     return str;
 }
 
-translatePigLatin("Consonant");
-//Pig Latin takes the first consonant (or consonant cluster) of an English word,
-// moves it to the end of the word and suffixes an "ay".
-
-//If a word begins with a vowel you just add "way"
-//to the end.
+translatePigLatin("Cccccccconsonant");
+// Pig Latin takes the first consonant (or consonant cluster) of an English
+// word, moves it to the end of the word and suffixes an "ay". If a word begins
+// with a vowel you just add "way" to the end.
